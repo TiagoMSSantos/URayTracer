ee4123af6a9287584cb52c177e9374970eee9fab
TiagoMSSantos <tiago92_net@hotmail.com>
2019-09-07 17:27:35 +0100

    Added Ray, Vector3 and Sphere classes.

---
+++ Sphere.scala
@@ -0,0 +1,21 @@
+package RayTracer
+import scala.math
+
+class Sphere(var radius : Float = 0, var center : Vector3, var emission : Vector3, var color : Vector3) {
+
+    def intersect(ray : Ray) {
+        var L = this.center - ray.origin
+        var tc = L * ray.direction
+        if (tc < 0) false
+
+        var distance = math.sqrt((tc * tc) - (L * L))
+        if (distance > sphere.radius) false
+
+        var t1c = math.sqrt((s.radius * s.radius) - (distance * distance))
+
+        var t1 = tc - t1c
+        var t2 = tc + t1c
+        true
+    }
+    
+}
---
+++ Vector3.scala
@@ -0,0 +1,69 @@
+package RayTracer
+import scala.math
+
+class Vector3(var x : Float = 0, var y : Float = 0, var z: Float = 0) {
+
+  def apply(x: Float, y: Float, z: Float): Vector3 = {
+    var vector = new Vector3(x, y, z)
+    vector
+  }
+
+  def getX() : Float = x
+  def getY() : Float = y
+  def getZ() : Float = z
+
+  def setX(x : Float) {
+    this.x = x
+  }
+
+  def getMagnitude () : Float = {
+    math.sqrt((this.x * this.x + this.y * this.y + this.z * this.z).toDouble).toFloat
+  }
+
+  def normalize() {
+    val magnitude = getMagnitude()
+    if (magnitude > 0) {
+      this.x = this.x / magnitude
+      this.y = this.y / magnitude
+      this.z = this.z / magnitude
+    }
+  }
+
+  def printVector() {
+    println("vector: " + this.x + ", " + this.y + ", " + this.z)
+  }
+
+  def +(other : Vector3) = {
+    Vector3(this.x + other.x, this.y + other.y, this.z + other.z)
+  }
+
+  def -(other : Vector3) = {
+    Vector3(this.x - other.x, this.y - other.y, this.z - other.z)
+  }
+
+  def *(other : Vector3) = {
+    this.x * other.x + this.y * other.y + this.z * other.z
+  }
+
+  def *(value : Float) = {
+    Vector3(this.x * value, this.y * value, this.z * value)
+  }
+
+  def crossProduct(other : Vector3) = {
+    Vector3(
+      this.y * other.z - this.z * other.y, 
+      this.z * other.x - this.x * other.z,
+      this.x * other.y - this.y * other.x
+    )
+  }
+}
+
+object Vector3 {
+  // convenience constructors
+  def apply(x: Float, y: Float, z: Float): Vector3 = {
+    var vector = new Vector3(x, y, z)
+    vector
+  }
+
+
+}
